25a0e38361885342bb4c3263a3e867e447f27379
jqmmy <judahaj23@gmail.com>
2026-02-02 14:34:56 -0500

    majorly improved fish boids

--- Scripts/fish.gd
+++ Scripts/fish.gd
@@ -4,13 +4,12 @@ extends Area3D
 var local_boids:Array[FishBoid] = []
 var velocity:Vector3
 var speed:float  = 0.1
-var movv:float = 1.1
-var target_influence:float = 0.1
-@export var predators:Array[Node3D]
+var movv:float = 2.0
+var target_influence:float = 1.0
+var obstacle:Node3D = null
 
 func _physics_process(delta: float) -> void:
-	
-#region boid algo
+	var target_point:Vector3 = get_parent().global_position
 	if local_boids:
 		var numOfBoids := local_boids.size()
 		var avgVel := Vector3.ZERO
@@ -23,40 +22,41 @@ func _physics_process(delta: float) -> void:
 
 		avgVel /= numOfBoids
 		velocity += (avgVel - velocity)/2
-
+		
 		avgPos /= numOfBoids
 		velocity += (avgPos - position)
-
+		if global_position.distance_to(target_point) > 7:
+			speed = move_toward(speed, 50, 1.0 * delta)
+		else:
+			speed = move_toward(speed, 5, 1.0 * delta)
+		
 		steerAway /= numOfBoids
 		velocity += (steerAway)
-#endregion
-	var target_point:Vector3 = get_parent().global_position
-	if global_position.distance_to(target_point) > 12:
+		
+	if global_position.distance_to(target_point) > 6:
 		var point_direction:Vector3
 		point_direction = global_position.direction_to(target_point) * target_influence
 		velocity += point_direction
-	elif global_position.distance_to(target_point) < 6.0:
-		var point_direction:Vector3
-		point_direction = target_point.direction_to(global_position) * target_influence
-		velocity += point_direction
-	
-	for pred in predators:
-		pass 
-		#code for avoiding stuff should be checking if close and 
-		#add a vector in the opposite direction
-	
-	
+
+	if obstacle:
+		velocity += global_position.direction_to(obstacle.global_position) * -target_influence
 	
 	velocity = velocity.normalized() * speed
 	
-	global_position += velocity
-
-
+	global_position += velocity * delta
 
 
 func _on_area_entered(area: Area3D) -> void:
-	local_boids.append(area)
-
+	if area.is_in_group("fish"):
+		local_boids.append(area)
 
 func _on_area_exited(area: Area3D) -> void:
-	local_boids.erase(area)
+	if area.is_in_group("fish"):
+		local_boids.erase(area)
+
+
+func _on_body_entered(body: Node3D) -> void:
+	obstacle = body
+
+func _on_body_exited(body: Node3D) -> void:
+	obstacle = null
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -5,11 +5,11 @@ const FISH = preload("uid://cagrfw40y8ip7")
 
 
 func _ready() -> void:
-	var fish_amount:int = 450
+	var fish_amount:int = 650
 	
 	for fish in fish_amount:
 		var boid:FishBoid = FISH.instantiate()
-		boid.position = Vector3(randf_range(-10, 10,), randf_range(0, 10), randf_range(-10, 10))
+		boid.position =  node_3d.position + Vector3(randf_range(-5, 5,), randf_range(0, 5), randf_range(-5, 5))
 		node_3d.add_child(boid)
 	
 
